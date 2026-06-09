135a0be47198d6d3fe1422404895b5b1a8586853
saralorranycontato13-cell <saralorrany.contato13@gmail.com>
2026-06-09 13:49:34 -0300

    jogador

--- jogo.gd
+++ jogo.gd
@@ -4,20 +4,19 @@ func _ready() -> void:
 	velocidade = 400
 
 func _process(delta: float) -> void:
-	# constantemente desloca o sprite pra esquerda
-	$fundo. position.x -= velocidade*delta
-	$fundo2.position.x -= velocidade*delta
-	if $fundo.position.x < -1152: # se o fundo sair de cena pela esquerda
-		$fundo.position.x = 1152 # coloque ele após a cena na direita
+	$fundo. position.x -= velocidade*delta*0.05
+	$fundo2.position.x -= velocidade*delta*0.05
+	$meioAzul.position.x -= velocidade*delta*0.06
+	$meioAzul2.position.x -= velocidade*delta*0.06
+	$meioPreto.position.x -= velocidade*delta*0.06
+	$meioPreto2.position.x -= velocidade*delta*0.06
+	if $fundo.position.x < -1152:
+		$fundo.position.x = 1152 
 		if $fundo2.position.x < -1152:
 			$fundo2.position.x = 1152
-	$chao.position.x -= velocidade*delta
-	$chao2.position.x -= velocidade*delta
-	if $chao.position.x < -1152: # se o fundo sair de cena pela esquerda
-		$chao. position.x = 1152 # coloque ele após a cena na direita
+		$chao.position.x -= velocidade*delta*2
+		$chao2.position.x -= velocidade*delta*2
+	if $chao.position.x < -1152: 
+		$chao. position.x = 1152 
 	if $chao2.position.x < -1152:
 		$chao2.position.x = 1152
-	$meioAzul.position.x   -= velocidade*delta*0.05
-	$meioPreto.position.x  -= velocidade*delta*0.05
-	$chao.position.x    -= velocidade*delta*2
-	$chao2.position.x   -= velocidade*delta*2
